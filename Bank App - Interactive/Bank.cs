3da8a91b46753b6bcf6d59affdf987e019a5bfab
Faris Zulfequar <zulfequf@shernet.sheridancollege.ca>
2025-03-05 14:37:28 -0500

    Added all the methods for the Bank class

--- Bank App - Interactive/Bank.cs
+++ Bank App - Interactive/Bank.cs
@@ -4,5 +4,52 @@ namespace BankApp;
 /// </summary>
 public class Bank
 {
+    #region Field Variables
+    /// <summary>
+    /// A list field variable to hold all the accounts
+    /// </summary>
+    private List<Account> _accountList;
+    #endregion
     
+    
+    #region Constructor
+    public Bank()
+    {
+        _accountList = new List<Account>();
+    }
+    #endregion
+    
+    
+    #region Methods
+    public Account OpenAccount()
+    {
+        return null;
+    }
+
+    public void LoadAccountData()
+    {
+        
+    }
+
+    public void SaveAccountData()
+    {
+        
+    }
+
+    public Account FindAccount(int acctNum)
+    {
+        return null;
+        
+    }
+
+    private void CreateDefaultAccounts()
+    {
+        
+    }
+
+    private int DetermineAccountNumber()
+    {
+        return 0;
+    }
+    #endregion
 }
